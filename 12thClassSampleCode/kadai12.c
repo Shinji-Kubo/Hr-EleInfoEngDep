51af20862b262d8dc093b062a10f314a4fc58ef8
Shinji Kubo <s.2001kubo@outlook.jp>
2021-07-13 10:58:01 +0900

    12thClass

--- 12thClassSampleCode/kadai12.c
+++ 12thClassSampleCode/kadai12.c
@@ -3,7 +3,40 @@
 void merge(int *A, int left, int mid, int right) {
 	int i = left;
 	int j = mid + 1;
-	int k = 0;	
+	int k = 0;
+	int *B;
+
+	while (i <= mid && j <= right) {
+		if (A[i] <= A[j]) {
+				B[k] = A[i];
+				i++;
+		} else {
+				B[k] = A[j];
+				j++;
+		}
+		k++;
+	}
+
+	if (i == mid+1) {
+		while (j <= right) {
+			B[k] = A[j];
+			j++;
+			k++;
+		}
+	} else {
+		while(i <= mid) {
+			B[k] = A[i];
+			i++;
+			k++;
+		}
+	}
+
+	k = 0;
+	for (i=left; i<right; i++) {
+		A[i] = B[k];
+		k++;
+	}
+
 }
 
 void merge_sort(int *A, int left, int right) {
