71520f1cbcdd1768d4c007771b798e18548de633
Shinji-Kubo <s.2001kubo@outlook.jp>
2021-07-13 10:48:00 +0900

    12thClass

--- 12thClassSampleCode/kadai12.c
+++ 12thClassSampleCode/kadai12.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 
+void merge(int *A, int left, int mid, int right) {
+	int i = left;
+	int j = mid + 1;
+	int k = 0;	
+}
+
+void merge_sort(int *A, int left, int right) {
+	if (left == right) {
+		return;
+	}
+
+	int mid = (left + right) / 2;
+
+	merge_sort(A, left, mid);
+	merge_sort(A, mid+1, right);
+	merge(A, left, mid, right);
+}
+
 main() {
 
 	return 0;
